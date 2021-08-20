b7f3a07d18831687983d3878f04f49fe76f4465a
sai gireesh <saigirishchinthakani.com>
2021-08-20 16:54:07 +0530

    initial checkin

---
+++ src/main/java/com/sai/devops/sai_maven/App.java
@@ -0,0 +1,13 @@
+package com.sai.devops.sai_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from sai!" );
+    }
+}
